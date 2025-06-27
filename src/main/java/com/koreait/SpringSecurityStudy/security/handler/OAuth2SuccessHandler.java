1563cbb47d9ef556666c76218f578ae746a95741
hdni2002 <hdni2003@gmail.com>
2025-06-27 21:48:39 +0900

    Feat:commit

--- src/main/java/com/koreait/SpringSecurityStudy/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/koreait/SpringSecurityStudy/security/handler/OAuth2SuccessHandler.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Optional;
 
+@Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     @Autowired
@@ -24,7 +26,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     @Autowired
     private UserRepository userRepository;
-
 
     @Autowired
     private JwtUtil jwtUtil;
@@ -40,20 +41,23 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         //provider, providerUserId 이미 연동된 사용자 정보가 있는지 DB 조회
         OAuth2User oAuth2User = oAuth2UserRepository.getOAuth2UserByProviderAndProviderUserId(provider, providerUserId);
 
-        //OAuth2 로그인을 통해 회원가입이 되지 않거나 아직 연동되지 않은 상태
-        if(oAuth2User == null){
-            //프론트로 provider와 providerUserId ,email 전달
-            response.sendRedirect("http://localhost:3000/auth/oauth2?provider=" + provider + "&providerUserid=" + providerUserId + "&email=" + email);
+        //OAuth2 로그인을 통해 회원가입이 되어있지 않거나 아직 연동되지 않은 상태
+        if(oAuth2User == null) {
+            //프론트로 provider와 providerUserId, email 전달
+            response.sendRedirect("http://localhost:3000/auth/oauth2?provider=" + provider + "&providerUserId=" + providerUserId + "&email=" + email);
+            return;
         }
 
-        //연동된 사용자가 있다면 ? => userId를 통해 회원 정보 조회
+        //연동된 사용자가 있다면? => userId를 통해 회원 정보 조회
         Optional<User> optionalUser = userRepository.getUserByUserId(oAuth2User.getUserId());
 
         //OAuth2 로그인을 통해 회원가입이나 연동을 진행한 경우
         String accessToken = null;
         if(optionalUser.isPresent()){
             accessToken = jwtUtil.generateAccessToken(Integer.toString(optionalUser.get().getUserId()));
-
         }
+
+        //최종적으로 accessToken을 쿼리 파라미터로 프론트에 전달
+        response.sendRedirect("http://localhost:3000/auth/oauth2/signin?accessToken=" + accessToken);
     }
 }
